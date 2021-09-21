b5215309cb56b6905bb4e53b4fc64fe0653b78fd
Bruno Silva Santana <ibrunoss@outlook.com>
2021-09-21 17:39:11 -0300

    model: QuestionModel class structure created

---
+++ model/question.ts
@@ -0,0 +1,34 @@
+export default class QuestionModel {
+  #id: number;
+  #statement: string;
+  #alternatives: any[];
+  #gotRight: boolean;
+
+  constructor(
+    id: number,
+    statement: string,
+    alternatives: any[],
+    gotRight: boolean
+  ) {
+    this.#id = id;
+    this.#statement = statement;
+    this.#alternatives = alternatives;
+    this.#gotRight = gotRight;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get statement() {
+    return this.#statement;
+  }
+
+  get alternatives() {
+    return this.#alternatives;
+  }
+
+  get gotRight() {
+    return this.#gotRight;
+  }
+}

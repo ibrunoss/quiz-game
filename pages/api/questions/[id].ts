fc053f039c335e4108ae8dbb4ab751cbae99ce58
Bruno Silva Santana <ibrunoss@outlook.com>
2021-09-23 17:57:46 -0300

    feat: add api/questions/:id

---
+++ pages/api/questions/[id].ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import questions from "../../../data/questions";
+import { QuestionModelInterface } from "../../../model/question";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const questionId = +req.query.id;
+  const filteredQuestion = questions.filter(
+    (question) => question.id === questionId
+  );
+
+  const question: QuestionModelInterface | false = filteredQuestion[0]
+    ? filteredQuestion[0].toObject()
+    : false;
+
+  if (question) {
+    res.status(200).json(question);
+  }
+
+  res.status(204).send(undefined);
+};

abfe3a5d21a9fa9d54918901ede11274098afaca
Bruno Silva Santana <ibrunoss@outlook.com>
2021-09-25 20:47:26 -0300

    component: add back content in Alternative

--- components/Alternative/Alternative.tsx
+++ components/Alternative/Alternative.tsx
@@ -17,17 +17,38 @@ const Alternative: React.FC<AlternativeProps> = (props) => {
     letterBg: backgroundColor,
     onResponse,
   } = props;
+
+  const renderCorrectAlternative = () => (
+    <div className={styles.correct}>
+      <div>Resposta correta</div>
+      <div className={styles.text}>{alternative.value}</div>
+    </div>
+  );
+
+  const renderWrongAlternative = () => (
+    <div className={styles.wrong}>
+      <div>Resposta errada</div>
+      <div className={styles.text}>{alternative.value}</div>
+    </div>
+  );
+
   return (
     <div className={styles.alternative} onClick={() => onResponse(id)}>
       <div className={styles.content}>
-        <div className={styles.front}>
-          <div className={styles.letter} style={{ backgroundColor }}>
-            {letter}
+        {alternative.clicked ? (
+          <div className={styles.back}>
+            {alternative.right
+              ? renderCorrectAlternative()
+              : renderWrongAlternative()}
           </div>
-          <div className={styles.text}>{alternative.value}</div>
-        </div>
-
-        <div className={styles.back}></div>
+        ) : (
+          <div className={styles.front}>
+            <div className={styles.letter} style={{ backgroundColor }}>
+              {letter}
+            </div>
+            <div className={styles.text}>{alternative.value}</div>
+          </div>
+        )}
       </div>
     </div>
   );

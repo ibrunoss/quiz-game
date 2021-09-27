e6b5871f2982f5292496df6a3e720890289ef9de
Bruno Silva Santana <ibrunoss@outlook.com>
2021-09-27 20:18:29 -0300

    component: Button created

---
+++ components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+import styles from "./Button.module.css";
+
+interface ButtonProps {
+  href: string;
+  text: string;
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const { href, text } = props;
+  return (
+    <Link href={href}>
+      <button className={styles.button}>{text}</button>
+    </Link>
+  );
+};
+
+export default Button;

9935d16aaa0e32334d4f4b89d085108f67ab1683
Bruno Silva Santana <ibrunoss@outlook.com>
2021-09-28 14:58:53 -0300

    refactor: home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,111 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
+
+import Quiz from "../components/Quiz/Quiz";
+import AlternativeModel from "../model/alternative";
+import QuestionModel, { QuestionModelInterface } from "../model/question";
+
+const questionInitial = new QuestionModel(1, "Enunciado da pergunta?", [
+  AlternativeModel.wrong("Alternativa Um"),
+  AlternativeModel.wrong("Alternativa Dois"),
+  AlternativeModel.wrong("Alternativa Três"),
+  AlternativeModel.correct("Alternativa Quatro"),
+]);
+
+const baseURL = (endpoint: string = "") => `/api${endpoint}`;
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <h1>Quiz Game</h1>
-    </div>
+  const [question, setQuestion] = useState(questionInitial);
+  const [questionsIds, setQuestionsIds] = useState<number[]>([]);
+  const [correctAnswerCount, setCorrectAnswerCount] = useState<number>(0);
+  const [wrongAnswerCount, setWrongAnswerCount] = useState<number>(0);
+
+  const router = useRouter();
+
+  const questionRef = useRef<QuestionModel>(question);
+
+  useEffect(() => {
+    questionRef.current = question;
+  }, [question]);
+
+  const getQuestionsIds = async () => {
+    const resp = await fetch(baseURL("/quiz"));
+    const ids: number[] = await resp.json();
+
+    setQuestionsIds(ids);
+  };
+
+  const getQuestion = async (questionId: number) => {
+    const resp = await fetch(baseURL(`/questions/${questionId}`));
+    const question: QuestionModelInterface = await resp.json();
+
+    setQuestion(QuestionModel.makeNew(question));
+  };
+
+  useEffect(() => {
+    getQuestionsIds();
+  }, []);
+
+  useEffect(() => {
+    questionsIds.length > 0 && getQuestion(questionsIds[0]);
+  }, [questionsIds]);
+
+  const style = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+  };
+
+  const nextQuestionId = (): number | undefined => {
+    const nextIndex = questionsIds.indexOf(question.id) + 1;
+
+    return questionsIds[nextIndex];
+  };
+
+  const nextStep = () => {
+    setTimeout(() => {
+      const nextIndex = nextQuestionId();
+
+      nextIndex ? getQuestion(nextIndex) : finish();
+    }, 250);
+  };
+
+  const finish = () =>
+    router.push({
+      pathname: "/result",
+      query: {
+        c: correctAnswerCount,
+        w: wrongAnswerCount,
+        t: correctAnswerCount + wrongAnswerCount,
+      },
+    });
+
+  const onResponse = (question: QuestionModel) => {
+    question.gotRight
+      ? setCorrectAnswerCount(correctAnswerCount + 1)
+      : setWrongAnswerCount(wrongAnswerCount + 1);
+
+    setQuestion(question);
+
+    nextStep();
+  };
+
+  const timeOut = () => {
+    const question = questionRef.current;
+
+    if (question.notAnswered) {
+      setWrongAnswerCount(wrongAnswerCount + 1);
+    }
+
+    nextStep();
+  };
+
+  return question ? (
+    <Quiz question={question} onResponse={onResponse} timeOut={timeOut} />
+  ) : (
+    <div>Loading...</div>
   );
 };
 
